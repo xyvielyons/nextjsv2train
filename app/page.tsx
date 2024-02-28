8115d9ade275c6a2bd1838c2b925b2642c39bb49
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-02-28 16:14:49 +0300

    changes v2

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+import { Lusitana } from 'next/font/google';
+import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -26,6 +28,23 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+
+          <Image 
+          src="/hero-desktop.png"
+          width={1000}
+          height={760}
+          className='hidden md:block'
+          alt="screenshots of the dashboard project showing desktop"
+          />
+          <Image
+          src="/hero-mobile.png"
+          width={560}
+          height={620}
+          className='block md:hidden'
+          alt="screenshot of the dashboard project showing mobile version"
+          ></Image>
+          
+        
         </div>
       </div>
     </main>
